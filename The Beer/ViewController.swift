9af14c86a08093b04628ef2c355f46e30e280514
Marcello Alves <marcello_doalves@hotmail.com>
2021-12-09 13:34:48 -0300

    Initial Commit

---
+++ The Beer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Beer
+//
+//  Created by Marcello Alves on 09/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
